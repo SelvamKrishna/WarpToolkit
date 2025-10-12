c5b927d26dfd15eb13ba5d4804d311e4e1808398
SelvamKrishna <selvam.krishna0510@gmail.com>
2025-10-12 10:13:03 +0800

    feat(log):  added warp::log::i_tag interface class support for polymorphic contextual logging - Introduced warp::log::i_tag which is a interface for all tags - Tags are used for adding context for the warp::log::sender - A tagged message looks like this ''' [APP][AUDIO_SYS] : Loaded 'blast-sfx.mp4' successfully '''

--- warp_log/warp_log.cpp
+++ warp_log/warp_log.cpp
@@ -1,17 +1,49 @@
 #include "warp_log.hpp"
 
+#include <cstddef>
+#include <mutex>
+
+static std::mutex g_console_mutex;
+
 void warp::log::internal::write_to_console(
 	std::ostream& os,
-	const std::string& pre,
-	const std::string& suf
+	std::string_view pre,
+	std::string_view msg
 ) {
-	os << pre << " : " << suf << "\n";
+	std::scoped_lock lock{g_console_mutex};
+
+	std::string buffer;
+	buffer.reserve(pre.size() + msg.size() + 4);
+
+	buffer.append(pre);
+	buffer.append(" : ");
+	buffer.append(msg);
+	buffer.append("\n");
+
+	os << buffer;
 }
 
-void warp::log::sender::log(level lvl, const std::string& msg) const noexcept {
-	internal::write_to_console(
-		internal::stream_from_level(lvl),
-		this->_context,
-		msg
-	);
+std::string warp::log::internal::cache_tag_vec(
+	const warp::log::tag_vec& tag_vec,
+	std::string_view delim
+) {
+	if (tag_vec.empty()) return {};
+
+	size_t total_size = (tag_vec.size() - 1) * delim.size();
+	for (const auto* TAG: tag_vec) {
+		if (TAG) total_size += TAG->get_str().size();
+	}
+
+	std::string result;
+	result.reserve(total_size);
+	result.append(tag_vec[0]->get_str());
+
+	for (size_t i = 1; i < tag_vec.size(); ++i)	{
+		if (tag_vec[i] == nullptr) [[unlikely]] continue;
+
+		result.append(delim);
+		result.append(tag_vec[i]->get_str());
+	}
+
+	return result;
 }
--- warp_log/warp_log.hpp
+++ warp_log/warp_log.hpp
@@ -5,18 +5,22 @@
 
 #define WARP_LOG_VER 1
 
-#ifdef _WIN32
-	#define WARP_LOG_API __declspec(dllexport)
-#else
-	#define WARP_LOG_API
+#ifndef WARP_TOOLKIT_API
+	#ifdef _WIN32
+		#define WARP_TOOLKIT_API __declspec(dllexport)
+	#else
+		#define WARP_TOOLKIT_API
+	#endif
 #endif
 
 #include <cstdint>
+#include <vector>
 #include <iostream>
+#include <string>
+#include <string_view>
+#include <utility>
 
 namespace warp::log {
-
-#pragma region /// Helpers
 
 enum class level : uint8_t {
 	INFO,
@@ -25,47 +29,51 @@ enum class level : uint8_t {
 	ERROR,
 };
 
+class WARP_TOOLKIT_API i_tag {
+public:
+	i_tag() = default;
+	virtual ~i_tag() = default;
+	[[nodiscard]] virtual std::string get_str() const noexcept = 0;
+};
+
+using tag_vec = std::vector<i_tag*>;
+
 namespace internal {
 
-WARP_LOG_API void write_to_console(std::ostream& os, const std::string& pre, const std::string& suf);
+WARP_TOOLKIT_API void write_to_console(std::ostream& os, std::string_view pre, std::string_view msg);
 
-WARP_LOG_API inline constexpr std::ostream& stream_from_level(level lvl) noexcept {
+[[nodiscard]] inline constexpr std::ostream& stream_from_level(level lvl) noexcept {
 	return (lvl == level::INFO || lvl == level::DEBUG) ? std::cout : std::cerr;
 }
 
-} /// internal
+WARP_TOOLKIT_API std::string cache_tag_vec(const tag_vec& tags, std::string_view delim = "");
+} /// namespace internal
 
-#pragma endregion /// Helpers
-
-class WARP_LOG_API sender final {
+class WARP_TOOLKIT_API sender final {
 private:
 	std::string _context;
 
 public:
-#pragma region /// C-tors & D-tors
-
-	explicit sender() noexcept = delete;
+	sender() = delete;
+	
 	explicit sender(std::string context) noexcept : _context(std::move(context)) {}
 
-	~sender() noexcept = default;
+	explicit sender(const i_tag& tag) noexcept : _context(tag.get_str()) {}
 
-#pragma endregion /// C-tors & D-tors
-#pragma region /// Copy & Move (Default)
+	explicit sender(const tag_vec& tags) noexcept : _context(internal::cache_tag_vec(tags)) {}
 
-	explicit constexpr sender(const sender &) noexcept = default;
-	sender &operator=(const sender &) noexcept = default;
+	inline void log(level lvl, std::string_view msg) const noexcept {
+		using namespace internal;
+		auto& os = stream_from_level(lvl);
+		write_to_console(os, _context, msg);
+	}
 
-	explicit constexpr sender(sender &&) noexcept = default;
-	sender &operator=(sender &&) noexcept = default;
-
-#pragma endregion /// Copy & Move (Default)
-#pragma region /// Core features
-
-	void log(level log_level, const std::string& msg) const noexcept;
-
-#pragma endregion /// Core features
+	inline void info(std::string_view msg) const noexcept { log(level::INFO, msg); }
+	inline void dbg(std::string_view msg)  const noexcept { log(level::DEBUG, msg); }
+	inline void warn(std::string_view msg) const noexcept { log(level::WARN, msg); }
+	inline void error(std::string_view msg) const noexcept { log(level::ERROR, msg); }
 };
 
-} /// namespace warp::log
+} // namespace warp::log
 
-#endif /// WARP_LOG
+#endif // WARP_LOG
