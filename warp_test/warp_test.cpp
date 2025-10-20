09ff44190e27a240e236f839584459e2d91b07f7
SelvamKrishna <selvam.krishna0510@gmail.com>
2025-10-20 14:36:59 +0800

    feat: Added Registry class
    - Registry allows collection handling
    - Logging system still in progess

--- warp_test/warp_test.cpp
+++ warp_test/warp_test.cpp
@@ -1,14 +1,15 @@
-#include "warp_test/warp_test.hpp"
 #include <format>
 #include <iostream>
 #include <string_view>
+
+#include "warp_test/warp_test.hpp"
 
 namespace warp::test {
 
 void Suite::_logTestCase(bool cond, std::string_view desc) noexcept {
   std::cout << std::format(
     ""
-    "    \033[34m[CASE]\033[0m"
+    "|---------->\033[34m[CASE]\033[0m"
     "{}"
     " : {}\n",
     (cond ? "\033[32m[PASS]\033[0m" : "\033[31m[FAIL]\033[0m"), desc
@@ -16,11 +17,14 @@ void Suite::_logTestCase(bool cond, std::string_view desc) noexcept {
 }
 
 Suite::Suite(std::string_view desc) noexcept {
-  std::cout << std::format("  \033[34m[SUITE]\033[0m : {} {}\n\n", desc, '{');
+  std::cout << std::format("    [-->\033[34m[SUITE]\033[0m : {}\n", desc);
 }
 
 Suite::~Suite() noexcept {
-  std::cout << std::format("\n  {} \033[33m[{}/{}]\033[0m\n\n", '}', _test_summary.getPassedCases(), _test_summary.getTotalCases());
+  std::cout << std::format(
+    "\n    [-->\033[33m[{}/{}]\033[0m\n",
+    _test_summary.getPassedCases(), _test_summary.getTotalCases()
+  );
 }
 
 void Suite::checkEq(bool cond, std::string_view desc) noexcept {
@@ -31,6 +35,34 @@ void Suite::checkEq(bool cond, std::string_view desc) noexcept {
 void Suite::checkNeq(bool cond, std::string_view desc) noexcept {
   _test_summary.addCase(!cond);
   Suite::_logTestCase(!cond, desc);
+}
+
+Registry::Registry() noexcept {
+  std::cout << std::format("\033[34m[REGISTRY]\033[0m\n");
+}
+
+Registry::~Registry() noexcept {
+  std::cout << std::format(
+    "\n\033[33m[{}/{}]\033[0m\n",
+    _test_summary.getPassedCases(), _test_summary.getTotalCases()
+  );
+}
+
+[[nodiscard]] Registry& Registry::addCollection(
+  std::string_view name,
+  std::vector<std::function<internal::Summary()>> suites
+) noexcept {
+  std::cout << std::format("[-->\033[34m[COLLECTION]\033[0m : {}\n", name);
+  internal::Summary collection_summary {};
+  for (const auto& TEST : suites) collection_summary += TEST();
+
+  std::cout << std::format(
+    "\n[-->\033[33m[{}/{}]\033[0m\n\n",
+    collection_summary.getPassedCases(), collection_summary.getTotalCases()
+  );
+
+  _test_summary += collection_summary;
+  return *this;
 }
 
 } // namespace warp::test
--- warp_test/warp_test.hpp
+++ warp_test/warp_test.hpp
@@ -12,6 +12,8 @@
 
 #include <cstdint>
 #include <string_view>
+#include <functional>
+#include <vector>
 
 namespace warp::test {
 
@@ -34,8 +36,10 @@ public:
   [[nodiscard]] constexpr uint64_t getPassedCases() const noexcept { return _passed_case; }
   [[nodiscard]] constexpr uint64_t getFailedCases() const noexcept { return _total_case - _passed_case; }
 
-  [[nodiscard]] constexpr float getPercentage() const noexcept {
-    return static_cast<float>(_passed_case * 100) / _total_case;
+  friend Summary& operator+=(Summary& self, const Summary& other) noexcept {
+    self._total_case += other._total_case;
+    self._passed_case += other._passed_case;
+    return self;
   }
 };
 
@@ -55,10 +59,31 @@ public:
   void checkEq(bool cond, std::string_view desc) noexcept;
   void checkNeq(bool cond, std::string_view desc) noexcept;
 
-  [[nodiscard]] constexpr bool close() const noexcept {
-    return _test_summary.getTotalCases() == _test_summary.getPassedCases();
+  [[nodiscard]] constexpr internal::Summary close() const noexcept {
+    return _test_summary;
   }
 };
+
+class WARP_TOOLKIT_API Registry final {
+private:
+  internal::Summary _test_summary;
+
+public:
+  explicit Registry() noexcept;
+  ~Registry() noexcept;
+
+  [[nodiscard]] Registry& addCollection(
+    std::string_view name,
+    std::vector<std::function<internal::Summary()>> suites
+  ) noexcept;
+
+  [[nodiscard]] constexpr int close() const noexcept {
+    return (_test_summary.getFailedCases() == 0) ? 0 : 1;
+  }
+};
+
+#define TEST_SUITE(fn_name) \
+  warp::test::internal::Summary fn_name()
 
 } // namespace warp::test
 
