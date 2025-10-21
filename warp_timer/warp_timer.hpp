0d65237a99d8eaf86b15cfc6cb47be49f5d0eef2
SelvamKrishna <selvam.krishna0510@gmail.com>
2025-10-21 22:19:49 +0800

    repositioned warp_test and converted warp_timer to hpp only

--- warp_timer/warp_timer.hpp
+++ warp_timer/warp_timer.hpp
@@ -117,10 +117,25 @@ protected:
   const TimeUnit _UNIT;
   bool _is_running {true};
 
-  [[nodiscard]] double _getTimeSinceStart() const noexcept;
-  [[nodiscard]] double _stopAndGetElapsed() noexcept;
+  [[nodiscard]] double _getTimeSinceStart() const noexcept {
+    const auto NOW = std::chrono::high_resolution_clock::now();
+    const double ELAPSED = std::chrono::duration<double, std::milli>(NOW - _start).count();
+    return internal::convertUnit(ELAPSED, TimeUnit::MilliSeconds, _UNIT);
+  }
 
-  [[nodiscard]] static double _measureCallableTimeMS(const std::function<void()>& callable) noexcept;
+  [[nodiscard]] double _stopAndGetElapsed() noexcept {
+    const double ELAPSED = _getTimeSinceStart();
+    _is_running = false;
+    _start = std::chrono::high_resolution_clock::now(); // reset start
+    return ELAPSED;
+  }
+
+  [[nodiscard]] static double _measureCallableTimeMS(const std::function<void()>& callable) noexcept {
+    const auto START = std::chrono::high_resolution_clock::now();
+    callable();
+    const auto STOP = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration<double, std::milli>(STOP - START).count();
+  }
 
 public:
   explicit Timer() noexcept
@@ -129,10 +144,18 @@ public:
   explicit Timer(std::string description, TimeUnit unit = TimeUnit::MilliSeconds) noexcept
   : _DESC {std::move(description)}, _start {std::chrono::high_resolution_clock::now()}, _UNIT {unit} {}
 
-  ~Timer() noexcept;
+  ~Timer() noexcept { if (_is_running) stop(); }
 
-  void start() noexcept;
-  void stop() noexcept;
+  void start() noexcept {
+    _is_running = true;
+    _start = std::chrono::high_resolution_clock::now();
+  }
+
+  void stop() noexcept  {
+    const double ELAPSED = _stopAndGetElapsed();
+    _logElapsed(_DESC, ELAPSED, _UNIT);
+  }
+
   void reset() noexcept { start(); }
 
   template <TimeUnit Target = TimeUnit::MilliSeconds>
@@ -159,8 +182,23 @@ class WARP_TOOLKIT_API HierarchyTimer final : public Timer {
 private:
   double _sub_task_measure {0.0};
 
-  void _logTimerStart() const noexcept;
-  void _subTaskImpl(std::string_view desc, double elapsed_ms, TimeUnit display_unit) noexcept;
+  void _logTimerStart() const noexcept {
+    std::cout << "\033[34m[TIMER]\033[0m : " << _DESC << " {\n\n";
+  }
+
+  void _subTaskImpl(std::string_view desc, double elapsed_ms, TimeUnit display_unit) noexcept {
+    {
+      _sub_task_measure += internal::convertUnit(elapsed_ms, TimeUnit::MilliSeconds, _UNIT);
+
+      std::cout
+        << "  \033[34m[TIMER][SUB]\033[0m"
+        << internal::formatElapsed(
+          internal::convertUnit(elapsed_ms, TimeUnit::MilliSeconds, display_unit),
+          display_unit
+        )
+        << " : " << desc << '\n';
+    }
+  }
 
 public:
   explicit HierarchyTimer() noexcept : Timer {} { _logTimerStart(); }
