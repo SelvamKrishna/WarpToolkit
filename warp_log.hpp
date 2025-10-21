aba0f02a6813597c7efe15ab5d7e32137d5f905c
SelvamKrishna <selvam.krishna0510@gmail.com>
2025-10-21 19:40:05 +0800

    warp_log integration into warp_test -pit-stop-1

--- warp_log.hpp
+++ warp_log.hpp
@@ -28,17 +28,17 @@ using LogTag = std::string;
 
 namespace tag_factory {
 
-  [[nodiscard]] static inline LogTag makeDefault(std::string_view text) noexcept { return std::string{text}; }
+[[nodiscard]] static inline LogTag makeDefault(std::string_view text) noexcept { return std::string{text}; }
 
-  [[nodiscard]] static inline LogTag makeColored(ANSIFore fg, std::string_view text) noexcept {
-    return std::format("\033[{}m{}\033[0m", static_cast<int>(fg), text);
-  }
+[[nodiscard]] static inline LogTag makeColored(ANSIFore fg, std::string_view text) noexcept {
+  return std::format("\033[{}m{}\033[0m", static_cast<int>(fg), text);
+}
 
 } // namespace tag_factory
 
 namespace internal {
 
-static std::mutex g_console_mutex;
+static std::mutex s_console_mutex;
 
 inline std::string levelToString(Level lvl) noexcept {
   switch (lvl) {
@@ -67,7 +67,7 @@ inline ANSIFore levelToColor(Level lvl) noexcept {
 }
 
 inline void writeToConsole(Level lvl, std::string_view prefix, std::string_view msg) {
-  std::scoped_lock lock{g_console_mutex};
+  std::scoped_lock lock{s_console_mutex};
   std::ostream& os = streamFromLevel(lvl);
 
   os << prefix
@@ -79,7 +79,7 @@ inline void writeToConsole(Level lvl, std::string_view prefix, std::string_view 
 [[nodiscard]] inline std::string cacheTagVec(const std::vector<LogTag>& tags, std::string_view delim = "") {
   if (tags.empty()) return {};
 
-  std::string result = tags[0];
+  std::string result {tags[0]};
   for (size_t i = 1; i < tags.size(); ++i) result.append(delim).append(tags[i]);
   return result;
 }
@@ -90,27 +90,31 @@ inline void writeToConsole(Level lvl, std::string_view prefix, std::string_view 
 class Sender final {
 private:
   std::string _context;
-  bool _allow_log_timestamp;
-  ANSIFore _time_stamp_color;
+  bool        _allow_log_timestamp;
+  ANSIFore    _time_stamp_color;
 
-  [[nodiscard]] std::string _getTimestamp() const noexcept {
-    char buf[sizeof("[HH:MM:SS]")] = {};
-    std::time_t t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-    std::tm tm_struct{};
+  [[nodiscard]] std::string _getTimestampTag() const noexcept {
+    char buf[sizeof("[HH:MM:SS]")] {};
+    std::time_t t {std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())};
+    std::tm tm_struct {};
+
     #ifdef _WIN32
       localtime_s(&tm_struct, &t);
     #else
       localtime_r(&t, &tm_struct);
     #endif
+
     std::strftime(buf, sizeof(buf), "[%H:%M:%S]", &tm_struct);
     return std::string{buf};
   }
 
   template <typename... Args>
   void _log(Level lvl, std::format_string<Args...> msg, Args&&... args) const {
-    std::string full_prefix = _allow_log_timestamp
-    ? tag_factory::makeColored(_time_stamp_color, _getTimestamp()) + _context
-    : _context;
+    std::string full_prefix {
+      _allow_log_timestamp
+        ? tag_factory::makeColored(_time_stamp_color, _getTimestampTag()) + _context
+        : _context
+    };
 
     internal::writeToConsole(lvl, full_prefix, std::format(msg, std::forward<Args>(args)...));
   }
@@ -119,14 +123,14 @@ public:
   Sender() = delete;
 
   explicit Sender(LogTag tag, bool log_timestamp = false) noexcept
-  : _context{std::move(tag)}
-  , _allow_log_timestamp{log_timestamp}
-  , _time_stamp_color{ANSIFore::White} {}
+  : _context             {std::move(tag)}
+  , _allow_log_timestamp {log_timestamp}
+  , _time_stamp_color    {ANSIFore::White} {}
 
   explicit Sender(const std::vector<LogTag>& tags, bool log_timestamp = false) noexcept
-  : _context{internal::cacheTagVec(tags)}
-  , _allow_log_timestamp{log_timestamp}
-  , _time_stamp_color{ANSIFore::White} {}
+  : _context             {internal::cacheTagVec(tags)}
+  , _allow_log_timestamp {log_timestamp}
+  , _time_stamp_color    {ANSIFore::White} {}
 
   /// Macro for creating log functions
   #define LOG_FN_IMPL(FN, LVL)  \
