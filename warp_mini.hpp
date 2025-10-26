c863d3716d1da5d9c1bedb64baa273dcddb19451
SelvamKrishna <selvam.krishna0510@gmail.com>
2025-10-26 18:36:00 +0800

    feat: WTEST macro

--- warp_mini.hpp
+++ warp_mini.hpp
@@ -39,25 +39,37 @@ static constexpr const char* LEVEL_STR[] {
 
 #if ENABLE_ANSI_COLOR_CODE
 static constexpr const char* COLOR_TABLE[] {
-  "\n\033[90m",
-  "\n\033[36m",
-  "\n\033[32m",
-  "\n\033[33m",
-  "\n\033[31m",
-  "\n\033[41m",
+  "\033[90m",
+  "\033[36m",
+  "\033[32m",
+  "\033[33m",
+  "\033[31m",
+  "\033[41m",
 };
 #endif
 
-[[nodiscard]] static constexpr inline std::string_view openLog(LogLevel level) noexcept {
+[[nodiscard]] static constexpr inline std::string_view openColor(LogLevel level) noexcept {
 #if ENABLE_ANSI_COLOR_CODE
   return COLOR_TABLE[level];
 #else
-  return "\n";
+  return "";
 #endif
 }
 
-[[nodiscard]] static constexpr inline const char* closeLog() noexcept {
+[[nodiscard]] static constexpr inline const char* closeColor() noexcept {
+#if ENABLE_ANSI_COLOR_CODE
   return "\033[0m : ";
+#else
+  return " : ";
+#endif
+}
+
+[[nodiscard]] static inline std::string colorText(int color_code, const char* text) noexcept {
+#if ENABLE_ANSI_COLOR_CODE
+  return std::format("\033[{}m{}\033[0m", color_code, text);
+#else
+  return text;
+#endif
 }
 
 [[nodiscard]] static inline std::string_view getTimestamp() noexcept {
@@ -93,16 +105,41 @@ static ResetTerminal s_reset_term {};
 
 /// --- MACROS ---
 
-#define WLOG(LVL) \
-  (LVL < L_WARN ? std::cout : std::cerr) \
-    << warp::mini::openLog(LVL) \
-    << warp::mini::getTimestamp() \
-    << warp::mini::LEVEL_STR[LVL] \
-    << warp::mini::closeLog() \
+/// `os <<` :
 
-#define WLOGT WLOG(L_TRACE)
-#define WLOGD WLOG(L_DEBUG)
-#define WLOGI WLOG(L_INFO)
-#define WLOGW WLOG(L_WARN)
-#define WLOGE WLOG(L_ERROR)
-#define WLOGF WLOG(L_FATAL)
+#define WLOG(LVL)                         \
+  (LVL < L_WARN ? std::cout : std::cerr)  \
+    << "\n"                               \
+    << warp::mini::openColor(LVL)         \
+    << warp::mini::getTimestamp()         \
+    << warp::mini::LEVEL_STR[LVL]         \
+    << warp::mini::closeColor()           \
+
+#define WLOGT  WLOG(L_TRACE)
+#define WLOGD  WLOG(L_DEBUG)
+#define WLOGI  WLOG(L_INFO)
+#define WLOGW  WLOG(L_WARN)
+#define WLOGE  WLOG(L_ERROR)
+#define WLOGF  WLOG(L_FATAL)
+
+/// `if (flag) os <<` :
+
+#define WLOG_IF(LVL, CONDITION) \
+  if (CONDITION) WLOG(LVL)
+
+#define WLOGT_IF(CONDITION)  WLOG_IF(L_TRACE, CONDITION)
+#define WLOGD_IF(CONDITION)  WLOG_IF(L_DEBUG, CONDITION)
+#define WLOGI_IF(CONDITION)  WLOG_IF(L_INFO, CONDITION)
+#define WLOGW_IF(CONDITION)  WLOG_IF(L_WARN, CONDITION)
+#define WLOGE_IF(CONDITION)  WLOG_IF(L_ERROR, CONDITION)
+#define WLOGF_IF(CONDITION)  WLOG_IF(L_FATAL, CONDITION)
+
+/// os << "a == b" << (a == b ? [PASS] : [FAIL]);
+
+#define WTEST(ACTUAL, EXPECTED) do { \
+  std::cout << warp::mini::colorText(34, "\n[TEST]") << " : "; \
+  (ACTUAL == EXPECTED) \
+    ? std::cout << warp::mini::colorText(32, "[PASS]") \
+    : std::cout << warp::mini::colorText(31, "[FAIL]"); \
+  std::cout << " : (" #ACTUAL ") == (" #EXPECTED ")\n"; \
+} while (0)
