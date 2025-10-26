6dd2408a5cbb0fa4c30d257968f330dfc7b1673a
SelvamKrishna <selvam.krishna0510@gmail.com>
2025-10-26 16:09:10 +0800

    feat: basic utility functions to help logging

--- warp_log/mini.hpp
+++ warp_log/mini.hpp
@@ -1,15 +1,80 @@
 #pragma once
 
+/// --- Configuration ---
+
+#define ENABLE_ANSI_COLOR_CODE (true)
+#define ENABLE_TIMESTAMP       (false)
+
+/// --- Includes ---
+
 #include <cstdint>
+#include <iostream>
 
-namespace warp::log {
+#if ENABLE_TIMESTAMP
+#include <chrono>
+#endif
 
-enum class MiniLevel : uint8_t {
-  Debug,
-  Info,
-  Warn,
-  Error,
-  Fatal,
+/// --- Utilities ---
+
+namespace warp::log::mini {
+
+enum MiniLogLevel : uint8_t {
+  L_TRACE,
+  L_DEBUG,
+  L_INFO,
+  L_WARN,
+  L_ERROR,
+  L_FATAL,
 };
 
-} // namespace warp::log
+static constexpr const char* LEVEL_STR[] {
+#if ENABLE_ANSI_COLOR_CODE
+  "[TRACE] : ",
+  "[DEBUG] : ",
+  "[INFO]  : ",
+  "[WARN]  : ",
+  "[ERROR] : ",
+  "[FATAL] : ",
+#else
+  "[TRACE] : ",
+  "[DEBUG] : ",
+  "[INFO]  : ",
+  "[WARN]  : ",
+  "[ERROR] : ",
+  "[FATAL] : ",
+#endif
+};
+
+[[nodiscard]] static inline std::string_view getTimestamp() noexcept {
+#if ENABLE_TIMESTAMP
+  std::time_t t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+  std::tm tm_struct{};
+
+  static char buf[sizeof("\n[HH:MM:SS]") - 1] {};
+
+#ifdef _WIN32
+  localtime_s(&tm_struct, &t);
+#else
+  localtime_r(&t, &tm_struct);
+#endif
+
+  std::strftime(buf, sizeof(buf), "\n[%H:%M:%S]", &tm_struct);
+  return buf;
+#else
+  return "\n";
+#endif
+}
+
+} // namespace warp::log::mini
+
+/// --- MACROS ---
+
+#define WLOG(LVL) \
+  (LVL < warp::log::mini::L_WARN ? std::cout : std::cerr) \
+    << warp::log::mini::getTimestamp() \
+    << warp::log::mini::LEVEL_STR[LVL] \
+
+inline void test() {
+  auto LVL = warp::log::mini::L_DEBUG;
+  WLOG(warp::log::mini::L_DEBUG) << "Hello World";
+}
